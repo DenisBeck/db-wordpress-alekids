e64e8a7c2cf87a913229734cf80c562406a61c81
DenisBeck <beckass86@gmail.com>
2022-03-08 13:45:13 +0800

    html and css completed

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+//     $('.top-slider__list').slick({
+//         infinite: false,
+//         speed: 300,
+//         slidesToShow: 1,
+//         adaptiveHeight: true,
+//         fade: true,
+//         cssEase: 'linear'
+//     });
+//     $('.shop-slider__list').slick({
+//         slidesToShow: 1,
+//         adaptiveHeight: true,
+//     });
+// });
